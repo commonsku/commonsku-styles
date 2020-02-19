7f294d783df775dfa395084e809278b073cf9aff
Bryan Leung <blkclkc@hotmail.com>
2020-02-19 09:08:11 -0500

    comment

--- examples/src/index.js
+++ examples/src/index.js
@@ -5,13 +5,13 @@ import { Button, Toggle, ToggleLink } from '../../src';
 
 const App = () => {
     return (
-        <div>
-        <Button>Test</Button>
-        <Toggle>
-            <ToggleLink selected>Test</ToggleLink>
-            <ToggleLink>Test</ToggleLink>
-        </Toggle>
-        </div>
+        <Fragment>
+            <Button>Test</Button>
+            <Toggle>
+                <ToggleLink selected>Test</ToggleLink>
+                <ToggleLink>Test</ToggleLink>
+            </Toggle>
+        </Fragment>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export {default as Select} from './Select'
+//export {default as Select} from './Select'
 export {default as Page} from './Page'
 export {default as Box} from './Box'
 export {default as Padding} from './Padding'
