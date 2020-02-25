f614e00677200e60505669a649f78a948b2e22ac
Haseeb Ahmad <haseeb@commonsku.com>
2020-02-25 15:21:37 -0500

    Moved StyledBox outside of Box component or else it'll cause problems like input inside it loosing focus onChange

--- src/@commonsku/styles/Box.tsx
+++ src/@commonsku/styles/Box.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 import { Row, Col } from 'react-flexbox-grid'
 import { H2 } from './Headings'
 
-const Box = (props: React.PropsWithChildren<{borderless?: boolean, padded?: boolean, title?:string, controls?:React.ReactNode }>) => {
+const StyledBox = styled.div<{borderless?: boolean, padded?: boolean}>`
+  background: white;
+  margin-top: 20px;
+  box-shadow: ${props => props.borderless ? 0 : `0 2px 4px rgba(0, 0, 0, 0.07)`};
+  border-radius: 5px;
+  padding: 20px ${props => props.padded ? 20 : 0}px;
+`
 
-  const StyledBox = styled.div<{borderless?: boolean, padded?: boolean}>`
-    background: white;
-    margin-top: 20px;
-    box-shadow: ${props => props.borderless ? 0 : `0 2px 4px rgba(0, 0, 0, 0.07)`};
-    border-radius: 5px;
-    padding: 20px ${props => props.padded ? 20 : 0}px;
-  `
+const Box = (props: React.PropsWithChildren<{borderless?: boolean, padded?: boolean, title?:string, controls?:React.ReactNode }>) => {
 
   return ( <StyledBox padded={props.padded} borderless={props.borderless}>
            {props.title || props.controls ?
