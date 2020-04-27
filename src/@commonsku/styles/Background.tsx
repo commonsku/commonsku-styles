ca27dac150695ba847d6025128bbb0a1869e7e7c
Xiaohan Zhang <xiaohan.zhang.xz@gmail.com>
2020-04-27 13:41:02 -0400

    Add custom size to Avatar

--- src/@commonsku/styles/Background.tsx
+++ src/@commonsku/styles/Background.tsx
@@ -3,8 +3,10 @@ import { getThemeColor } from './Theme';
 import { SharedStyles, SharedStyleTypes } from './SharedStyles'
 
 export const Background = styled.div<{padded?: boolean, fillWindow?:boolean}&SharedStyleTypes>`
-  background: ${props => getThemeColor(props, 'bggray')};
-  padding: 20px ${props => props.padded ? 20 : 0}px;
-  min-height: ${props => props.fillWindow ? "100vh" : 0};
-  ${SharedStyles}
+  &&& {
+    background: ${props => getThemeColor(props, 'bggray')};
+    padding: 20px ${props => props.padded ? 20 : 0}px;
+    min-height: ${props => props.fillWindow ? "100vh" : 0};
+    ${SharedStyles}
+  }
 `
