05b92b39d012b744d9fe930bf9376b4a074b74c2
Xiaohan Zhang <xiaohan.zhang.xz@gmail.com>
2020-04-27 13:41:02 -0400

    Add custom size to Avatar

--- src/@commonsku/styles/Label.tsx
+++ src/@commonsku/styles/Label.tsx
@@ -3,12 +3,14 @@ import { getThemeColor } from './Theme';
 import { SharedStyles, SharedStyleTypes } from './SharedStyles';
 
 const Label = styled.label<SharedStyleTypes>`
-  font-family: 'skufont-medium', sans-serif;
-  color: ${props => getThemeColor(props, 'textlabel')};
-  font-size: 1rem;
-  font-weight: 400;
-  width: 100%;
-  ${SharedStyles}
+  &&& {
+    font-family: 'skufont-medium', sans-serif;
+    color: ${props => getThemeColor(props, 'textlabel')};
+    font-size: 1rem;
+    font-weight: 400;
+    width: 100%;
+    ${SharedStyles}
+  }
 `;
 
 export {Label};
