779ae353ee23ba1340076f3a1135d2aa2952af61
Haseeb Ahmad <haseeb@commonsku.com>
2020-05-08 12:21:49 -0400

    Fixed export Badge and css

--- src/@commonsku/styles/Badge.tsx
+++ src/@commonsku/styles/Badge.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { colors } from './Theme'
 import { SharedStyleTypes, SharedStyles } from './SharedStyles'
 
-export const StyledBadge = styled.span<{color?:string} & SharedStyleTypes>`
+export const Badge = styled.span<{color?:string} & SharedStyleTypes>`
   display: inline-block;
   min-width: 10px;
   padding: 3px 7px;
@@ -14,7 +14,7 @@ export const StyledBadge = styled.span<{color?:string} & SharedStyleTypes>`
   white-space: nowrap;
   vertical-align: middle;
   background-color: ${p => p.color === 'cta' ? colors.cta : p.color==='grey' ? colors.primary0 : colors.primary};
-  color: ${p => p.color==='grey' ? colors.primary100 : colors.white}
+  color: ${p => p.color==='grey' ? colors.primary100 : colors.white};
   border-radius: 10px;
   ${SharedStyles}
 `;
