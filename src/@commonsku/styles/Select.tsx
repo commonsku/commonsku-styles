0b424ac2dbea66ece68499332ce0015579bc97f2
Haseeb Ahmad <haseeb@commonsku.com>
2020-05-09 21:28:09 -0400

    Added creatable selects

--- src/@commonsku/styles/Select.tsx
+++ src/@commonsku/styles/Select.tsx
@@ -1,32 +1,37 @@
 import React from 'react'
-import Select, { Props as SelectProps, StylesConfig } from 'react-select'
+import Select, { Props as SelectProps, Styles, Theme } from 'react-select'
+import CreatableSelect, { Props as CreatableSelectProps } from 'react-select/creatable'
 import { getThemeColor, colors } from './Theme';
 import {Label} from './Label'
 
-type SKUSelectProps = {
+type AssitionalSKUSelectProps = {
   noMargin?: boolean,
   error?: boolean,
   menuRelative?: boolean, // fix for scroll menu inside scroll container like popup
   inPopup?:boolean,
-} & SelectProps
+}
 
-const SKUSelect = ({noMargin, menuRelative, inPopup, ...props}: SKUSelectProps) => <Select 
-  {...(inPopup ? {
-    menuPlacement: 'auto',
-    menuPosition: 'fixed',
-    menuPortalTarget: document.body,
-  } : {})}
-  styles={{
+type SKUSelectProps = AssitionalSKUSelectProps & SelectProps
+
+const popupStyles: SelectProps = {
+  menuPlacement: 'auto',
+  menuPosition: 'fixed',
+  menuPortalTarget: document.body,
+}
+
+function skuSelectStyles(props: SKUSelectProps | CreatableSelectProps<{[key: string]: any}>): Styles {
+  return {
     option: (provided, state) => ({
       ...provided,
       borderBottom: 'none',
       padding: 10
     }),
-    input: (provided, state) => ({
+    input: (provided, state) => {
+      return {
       ...provided,
       height: 'auto',
       borderColor: props.error ? getThemeColor(props, 'special3'): getThemeColor(props, 'inputBorder', 'primary')
-    }),
+    }},
     control: (provided, state) => ({
       ...provided,
       marginBottom: (props.noMargin ? 0 : '1rem'),
@@ -51,24 +56,35 @@ const SKUSelect = ({noMargin, menuRelative, inPopup, ...props}: SKUSelectProps) 
       const transition = 'opacity 300ms';
       return { ...provided, opacity, transition };
     }
-  }}
-  theme={theme => ({
-    ...theme,
-    borderRadius: 5,
-    colors: {
-      ...theme.colors,
-      primary25: colors.primary0,
-      primary75: colors.primary0,
-      primary50: colors.primary10,
-      primary: colors.primary,
-      neutral20: colors.inputBorder,
-      neutral30: colors.inputBorder,
-      neutral80: colors.textbody,
-      neutral90: colors.textbody
-    },
-  })}
-  {...props}
-/>;
+  };
+}
+
+const skuSelectTheme = (theme: Theme) => ({
+  ...theme,
+  borderRadius: 5,
+  colors: {
+    ...theme.colors,
+    primary25: colors.primary0,
+    primary75: colors.primary0,
+    primary50: colors.primary10,
+    primary: colors.primary,
+    neutral20: colors.inputBorder,
+    neutral30: colors.inputBorder,
+    neutral80: colors.textbody,
+    neutral90: colors.textbody
+  },
+})
+
+const SKUSelect = ({noMargin, menuRelative, inPopup, error, ...props}: SKUSelectProps) =>
+  <Select 
+    {...(inPopup ? popupStyles : {})}
+    noMargin={noMargin}
+    menuRelative={menuRelative}
+    error={error}
+    styles={skuSelectStyles(props)}
+    theme={skuSelectTheme}
+    {...props}
+  />;
 
 const LabeledSelect = ({ parentStyle, ...props }: SKUSelectProps & {parentStyle?:object}) => {
   return (
@@ -79,4 +95,28 @@ const LabeledSelect = ({ parentStyle, ...props }: SKUSelectProps & {parentStyle?
   )
 }
 
-export {SKUSelect as Select, LabeledSelect};
+
+const SKUCreatableSelect = ({noMargin, menuRelative, inPopup, ...props}: AssitionalSKUSelectProps & CreatableSelectProps<{[key: string]: any}>) =>
+  // @ts-ignore
+  <CreatableSelect 
+    {...(inPopup ? popupStyles : {})}
+    styles={skuSelectStyles(props)}
+    theme={skuSelectTheme}
+    {...props}
+  />;
+
+const LabeledCreatableSelect = ({ parentStyle, ...props }: AssitionalSKUSelectProps & CreatableSelectProps<{[key: string]: any}> & {parentStyle?:object}) => {
+  return (
+    <div style={parentStyle}>
+      <Label htmlFor={props.name}>{props.label}</Label>
+      <SKUCreatableSelect {...props}/>
+    </div>
+  )
+}
+
+export {
+  SKUSelect as Select,
+  LabeledSelect,
+  SKUCreatableSelect as CreatableSelect,
+  LabeledCreatableSelect,
+};
