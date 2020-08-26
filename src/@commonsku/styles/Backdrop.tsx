8debeced2a3c299f7de777f310daee4b32bc6303
Haseeb Ahmad <haseeb@commonsku.com>
2020-08-26 13:24:20 -0400

    added transition fad effect

--- src/@commonsku/styles/Backdrop.tsx
+++ src/@commonsku/styles/Backdrop.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
-export const Backdrop = styled.div`
+type BackdropTypes = {
+    animationDuration?: number,
+}
+
+export const Backdrop = styled.div<BackdropTypes>`
     position: fixed;
     width: 100%;
     height: 100%;
     background-color: rgba(67, 83, 91, .3);
     z-index: 100;
+    transition: all ${p => ((p.animationDuration || 300)/1000)}s ease-in-out;
+    -moz-transition: all ${p => ((p.animationDuration || 300)/1000)}s ease-in-out;
+    -webkit-transition: all ${p => ((p.animationDuration || 300)/1000)}s ease-in-out;
+    opacity: 0.8;
 `;
