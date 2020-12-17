cdf8cf479866591b955c07885e985e5d47cd4a79
Haseeb Ahmad <haseeb@commonsku.com>
2020-12-17 09:39:09 -0500

    - fixed select types errors - updated datepicker styles

--- src/@commonsku/styles/Datepicker.tsx
+++ src/@commonsku/styles/Datepicker.tsx
@@ -2,6 +2,27 @@ import React from 'react';
 import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import { Input, InputProps } from './Input';
 
+import "react-datepicker/dist/react-datepicker.css";
+import { CalendarIcon } from './icons';
+
+export const CustomDateInput = ({ error, noMargin, onClick, ...props }: InputProps) => {
+
+  return (<>
+    <Input
+      noMargin={noMargin}
+      error={error}
+      style={{ marginRight: '2rem', display: 'inline-block' }}
+      className="react-datepicker-ignore-onclickoutside" 
+      type="text"
+      autoComplete="off"
+      {...props}
+    />
+    <span style={{ fontStyle: 'normal', cursor: 'pointer', position: 'absolute', top: '8px', right: '5px' }} onClick={onClick}>
+      <CalendarIcon style={{ width: '1.9rem', verticalAlign: 'middle', }} />
+    </span>
+  </>);
+}
+
 export function Datepicker({
   noMargin,
   error,
@@ -9,28 +30,24 @@ export function Datepicker({
   customInput,
   locale='en',
   todayButton='Today',
-  placeholder='yyyy-mm-dd',
-  dateFormat='yyyy-mm-dd',
-  isClearable=true,
-  peekNextMonth=true,
+  placeholder='YYYY-MM-DD',
+  dateFormat='YYYY-MM-DD',
+  isClearable=false,
   showMonthDropdown=true,
   showYearDropdown=true,
   dropdownMode="select",
-  timeInputLabel="Time:",
-  showTimeInput,
   ...props
 }: ReactDatePickerProps & InputProps & { value?: Date | null }) {
   return (
       //@ts-ignore
       <DatePicker
-        isClearable
         locale={locale}
-        selected={value}
+        selected={value || props.selected}
         todayButton={todayButton}
-        customInput={customInput || <Input noMargin={noMargin} error={error} />}
+        customInput={customInput || <CustomDateInput noMargin={noMargin} error={error} />}
         dateFormat={dateFormat}
         placeholderText={placeholder}
-        peekNextMonth={peekNextMonth}
+        isClearable={isClearable}
         showMonthDropdown={showMonthDropdown}
         showYearDropdown={showYearDropdown}
         dropdownMode={dropdownMode}
