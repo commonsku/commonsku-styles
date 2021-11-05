7188050e2a9902f05e3eeaacba1c956f9cd559db
Haseeb Ahmad <haseeb@commonsku.com>
2021-11-05 15:47:53 -0400

    exported icon label and container for input

--- src/@commonsku/styles/Input.tsx
+++ src/@commonsku/styles/Input.tsx
@@ -8,7 +8,7 @@ import {Label} from './Label'
 
 export type InputProps = StyledComponentProps<'input', any, {}, never> & {noMargin?: boolean, error?:boolean, isPercent?:boolean};
 
-const IconLabel = styled.div<InputProps>`
+export const IconLabel = styled.div<InputProps>`
   box-sizing: border-box;
   width: 40px;
   height: ${p => p.error ? 38 : 36}px;
@@ -22,7 +22,7 @@ const IconLabel = styled.div<InputProps>`
   line-height: 1.5rem;
 `
 
-const IconLabelContainer = styled.div`
+export const IconLabelContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
