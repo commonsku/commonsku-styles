fa94dbba69926680a9ca9dc7ac94b5ff6fda23ac
Haseeb Ahmad <haseeb@commonsku.com>
2021-11-05 12:44:08 -0400

    updated

--- src/@commonsku/styles/globalStyles.ts
+++ src/@commonsku/styles/globalStyles.ts
@@ -4,7 +4,7 @@ import { createGlobalStyle } from 'styled-components';
 //This should be on a class instead of body?
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: ${props => _.get(props, ['theme', 'fontFamily'])};
+    font-family: ${props => _.get(props, ['theme', 'fontFamily'], '"museo-sans","Helvetica Neue",Helvetica,Roboto,Arial,sans-serif')};
   }
 `;
 
