56190e4cf14476b7ceaafa0b5b73b63deab93158
Haseeb Ahmad <haseeb@commonsku.com>
2021-12-06 14:44:20 -0500

    updated cal header

--- src/@commonsku/styles/calendar/TasksCalendarHeader.tsx
+++ src/@commonsku/styles/calendar/TasksCalendarHeader.tsx
@@ -9,6 +9,7 @@ import { Text } from '../Text';
 import {GearIcon, NextPrevIcon} from '../icons';
 import {LabeledCheckbox} from '../Input';
 import {Dropdown} from '../Dropdown';
+import { Button, TabType } from '..';
 
 const WeekNav = styled(Text)`
   display: inline-block;
@@ -24,14 +25,18 @@ export type TasksCalendarHeaderProps = {
     currentWeek: number;
     selectedDate: Date;
     weekendsCheckbox: ReactNode;
-    [key: string]: any;
+    showAddTaskBtn?: boolean;
+    onClickAddTask?: VoidFunction;
+    tabs?: TabType[],
 };
 export const TasksCalendarHeader = ({
     onPrevWeek,
     onNextWeek,
     currentMonth,
-    tabs,
-    weekendsCheckbox
+    tabs=[],
+    weekendsCheckbox,
+    showAddTaskBtn=true,
+    onClickAddTask,
 }: React.PropsWithChildren<TasksCalendarHeaderProps>) => {
     return (
         <HeaderWrapper style={{padding: "0.5rem"}}>
@@ -39,16 +44,15 @@ export const TasksCalendarHeader = ({
                 <Tabs size="small" tabs={tabs} />
             </Col>
             <Col end xs md={3} style={{fontSize: '0.8em'}}>
-                        <Dropdown icon={<GearIcon width="25" color="#02C0DA" />}>
-                            <Row>
-                                {weekendsCheckbox}
-                            </Row>
-                        </Dropdown>
-                        <WeekNav style={{cursor: 'pointer', color: colors.primary, }} onClick={onPrevWeek}><NextPrevIcon color={"#02C0DA"} width={".8rem"}/></WeekNav>
-                        <WeekNav style={{ color: colors.disabledButton, verticalAlign: "middle"}}>
-                            {currentMonth ? format(currentMonth, "MMM yyyy") : ''}
-                        </WeekNav>
-                        <WeekNav style={{cursor: 'pointer', color: colors.primary, }} onClick={onNextWeek}><NextPrevIcon color={"#02C0DA"} width={".8rem"} next/></WeekNav>
+                {showAddTaskBtn ? <Button onClick={onClickAddTask} style={{marginRight: 10,}}>+ Add Task</Button> : null}
+                <Dropdown icon={<GearIcon width="25" color="#02C0DA" />}>
+                    <Row>{weekendsCheckbox}</Row>
+                </Dropdown>
+                <WeekNav style={{cursor: 'pointer', color: colors.primary, }} onClick={onPrevWeek}><NextPrevIcon color={"#02C0DA"} width={".8rem"}/></WeekNav>
+                <WeekNav style={{ color: colors.disabledButton, verticalAlign: "middle"}}>
+                    {currentMonth ? format(currentMonth, "MMM yyyy") : ''}
+                </WeekNav>
+                <WeekNav style={{cursor: 'pointer', color: colors.primary, }} onClick={onNextWeek}><NextPrevIcon color={"#02C0DA"} width={".8rem"} next/></WeekNav>
             </Col>
         </HeaderWrapper>
     );
