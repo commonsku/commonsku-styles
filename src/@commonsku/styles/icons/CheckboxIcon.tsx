cc83acef65c9b79d312b0c672812f8e333ce9785
Musaa Sami <musaa@commonsku.com>
2022-04-25 11:45:53 -0400

    updated CheckboxIcon

--- src/@commonsku/styles/icons/CheckboxIcon.tsx
+++ src/@commonsku/styles/icons/CheckboxIcon.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
-import colors from '../colors';
+import SVG, { SVGIconProps } from './SvgIcon';
+import { teal } from '../colors';
 
-export default function Icon({
-    width=20,
-    height=21,
-    fill=colors.primary1.main,
-    style={},
+type CheckboxIconProps = SVGIconProps;
+export default function CheckboxIcon({
+    color=teal.main,
+    size="medium",
+    altText="Unchecked Checkbox",
     ...props
-}: React.PropsWithChildren<{
-    width?: number|string,
-    height?: number|string,
-    fill?: string,
-    style?: React.CSSProperties,
-}>) {
-    return (
-        <svg
-            width={width}
-            height={height}
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            style={style}
-            {...props}
-        >
-            <path
-                d="M17.583.75H2.417A2.173 2.173 0 0 0 .25 2.917v15.166c0 1.192.975 2.167 2.167 2.167h15.166a2.173 2.173 0 0 0 2.167-2.167V2.917A2.173 2.173 0 0 0 17.583.75Zm0 17.333H2.417V2.917h15.166v15.166ZM16.49 7.25l-1.527-1.538-7.14 7.139-2.795-2.784-1.538 1.527 4.334 4.323 8.666-8.667Z"
-                fill={fill}
-            />
-        </svg>
-    );
+}: CheckboxIconProps) {
+    return <SVG size={size} aria-labelledby="CheckboxIcon" {...props}>
+        <path fill="none" d="M0 0h24v24H0z" />
+        <title id="CheckboxIcon" >{altText}</title>
+        <path
+            d="M17.583.75H2.417A2.173 2.173 0 0 0 .25 2.917v15.166c0 1.192.975 2.167 2.167 2.167h15.166a2.173 2.173 0 0 0 2.167-2.167V2.917A2.173 2.173 0 0 0 17.583.75Zm0 17.333H2.417V2.917h15.166v15.166ZM16.49 7.25l-1.527-1.538-7.14 7.139-2.795-2.784-1.538 1.527 4.334 4.323 8.666-8.667Z"
+            fill={color}
+        />
+    </SVG>
 }
