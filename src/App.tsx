2016d96fd0f1a89896451943f5296309027494bc
Ilya Yakubovich <ilya.yakubovich@gmail.com>
2022-09-08 14:57:27 -0400

    icon updates

--- src/App.tsx
+++ src/App.tsx
@@ -2185,6 +2185,7 @@ const App = () => {
                     <IconsShowcase Icons={[<icons.CreditCardIcon />]} name="CreditCardIcon"/>
                     <IconsShowcase Icons={[<icons.ShopIcon />]} name="ShopIcon"/>
                     <IconsShowcase Icons={[<icons.HistoryIcon />]} name="HistoryIcon"/>
+                    <IconsShowcase Icons={[<icons.HandleIcon />]} name="HandleIcon"/>
 
                   </IconContainer>
               </demo.InnerContainer>
