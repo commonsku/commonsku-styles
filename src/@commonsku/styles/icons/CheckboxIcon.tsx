fb53cdbae8e228e6a106f07e53f5ca72b95fa167
Ilya Yakubovich <ilya@Ilyas-MBP-2.ht.home>
2022-09-14 14:50:30 -0400

    Added an indeterminate state to CheckboxIcon and LabeledCheckbox, and removed max-width from LightIndicator

--- src/@commonsku/styles/icons/CheckboxIcon.tsx
+++ src/@commonsku/styles/icons/CheckboxIcon.tsx
@@ -5,9 +5,10 @@ import SVG, { SVGIconProps } from './SvgIcon';
 type CheckboxIconProps = SVGIconProps & {
     selected?: boolean, 
     hover?: boolean, 
-    hoverColor?: string;
+    hoverColor?: string,
     disabled?: boolean,
-    disabledColor?: string
+    disabledColor?: string,
+    indeterminate?: boolean
 };
 
 export default function CheckboxIcon({
@@ -18,6 +19,7 @@ export default function CheckboxIcon({
     hover=false,
     selected=false,
     disabled=false,
+    indeterminate=false,
     altText="Select",
     ...props
 }: CheckboxIconProps) {
@@ -25,7 +27,9 @@ export default function CheckboxIcon({
     selected ? altText="Unselect" : altText="Select";
 
     const renderPath = React.useMemo(() => {
-        if (selected === true) {
+        if (indeterminate) {
+            return <path d="M19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3ZM16 13H8C7.45 13 7 12.55 7 12C7 11.45 7.45 11 8 11H16C16.55 11 17 11.45 17 12C17 12.55 16.55 13 16 13Z" fill={disabled ?  disabledColor : color} />
+        } else if (selected) {
            return <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2Zm-8.29 13.29a.996.996 0 0 1-1.41 0L5.71 12.7a.996.996 0 1 1 1.41-1.41L10 14.17l6.88-6.88a.996.996 0 1 1 1.41 1.41l-7.58 7.59Z" fill={disabled ?  disabledColor : color} />
         } else if (hover === true && !disabled ) {
             return<><path
--- src/App.tsx
+++ src/App.tsx
@@ -1159,6 +1159,9 @@ const App = () => {
 
                   <LabeledCheckbox label="Relish" checked={true} disabled />
 
+                  <LabeledCheckbox label="Indeterminate" indeterminate={true} />
+
+
                 </ButtonsGroup>
                 {DemoCodeBlock({style:{marginBottom: "40px"}, code: 
 `<LabeledCheckbox label="Mustard"  checked={mustard} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { toggleMustard(!mustard); }} />
