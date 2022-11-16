a457c5c4e5660000fde7ac362d039df777a56007
Eric Duncan <102484574+ESDUNCAN@users.noreply.github.com>
2022-11-16 17:01:29 -0500

    final touchups of indents in toggleDemo

--- src/demo/ToggleDemo.tsx
+++ src/demo/ToggleDemo.tsx
@@ -10,33 +10,33 @@ export function ToggleDemo() {
   return <div style={{display: 'flex', flexDirection: 'column', marginBottom: '24px'}}>
     
     <demo.SmallLabel>Small Active Toggle</demo.SmallLabel>
-      <Toggle size="small" mr={24} mb={24} onClick={() => setSelected(!selected)}>
-        <ToggleLink selected={selected}>Active</ToggleLink>
-        <ToggleLink selected={!selected}>Inactiveeeeeeeee</ToggleLink>
-      </Toggle>
-      {DemoCodeBlock({code:
+    <Toggle size="small" mr={24} mb={24} onClick={() => setSelected(!selected)}>
+      <ToggleLink selected={selected}>Active</ToggleLink>
+      <ToggleLink selected={!selected}>Inactiveeeeeeeee</ToggleLink>
+    </Toggle>
+    {DemoCodeBlock({code:
 `<Toggle size="small" onClick={() => setSelected(!selected)}>
     <ToggleLink selected={!selected} >Active</ToggleLink>
     <ToggleLink selected={selected}>Inactiveeeeeeeee</ToggleLink>
 </Toggle>`})}
   
     <demo.SmallLabel>Medium Toggle</demo.SmallLabel>
-      <Toggle size="medium" mr={24} mb={24}>
-        <ToggleLink>Active</ToggleLink>
-        <ToggleLink selected>Inactive</ToggleLink>
-      </Toggle>
-      {DemoCodeBlock({code:
+    <Toggle size="medium" mr={24} mb={24}>
+      <ToggleLink>Active</ToggleLink>
+      <ToggleLink selected>Inactive</ToggleLink>
+    </Toggle>
+    {DemoCodeBlock({code:
 `<Toggle size="medium">
     <ToggleLink>Active</ToggleLink>
     <ToggleLink selected>Inactive</ToggleLink>
 </Toggle>`})}
 
     <demo.SmallLabel>Large Toggle</demo.SmallLabel>
-      <Toggle size="large" mr={24} mb={24}>
-        <ToggleLink selected >Active</ToggleLink>
-        <ToggleLink >Inactive</ToggleLink>
-      </Toggle>
-      {DemoCodeBlock({code:
+    <Toggle size="large" mr={24} mb={24}>
+      <ToggleLink selected >Active</ToggleLink>
+      <ToggleLink >Inactive</ToggleLink>
+    </Toggle>
+    {DemoCodeBlock({code:
 `<Toggle size="large">
     <ToggleLink selected>Active</ToggleLink>
     <ToggleLink>Inactive</ToggleLink>
@@ -50,33 +50,33 @@ export function ToggleWithStetchDemo(){
   return <div style={{display: 'flex', flexDirection: 'column', marginBottom: '24px'}}>
 
     <demo.SmallLabel>Small Active Toggle with stretch prop</demo.SmallLabel>
-      <Toggle size="small" stretch mr={24} mb={24} onClick={() => setSelected(!selected)}>
-        <ToggleLink selected={selected} stretch >Active</ToggleLink>
-        <ToggleLink selected={!selected} stretch >Inactive</ToggleLink>
-      </Toggle>
-      {DemoCodeBlock({code: 
+    <Toggle size="small" stretch mr={24} mb={24} onClick={() => setSelected(!selected)}>
+      <ToggleLink selected={selected} stretch >Active</ToggleLink>
+      <ToggleLink selected={!selected} stretch >Inactive</ToggleLink>
+    </Toggle>
+    {DemoCodeBlock({code: 
 `<Toggle size="small" stretch onClick={() => setSelected(!selected)}>
     <ToggleLink selected={!selected} stretch >Active</ToggleLink>
     <ToggleLink selected={selected} stretch >Inactive</ToggleLink>
 </Toggle>`})}
 
     <demo.SmallLabel>Medium Toggle with stretch prop</demo.SmallLabel>
-      <Toggle size="medium" stretch mr={24} mb={24}>
-        <ToggleLink stretch>Active</ToggleLink>
-        <ToggleLink selected stretch>Inactive</ToggleLink>
-      </Toggle>
-      {DemoCodeBlock({code: 
+    <Toggle size="medium" stretch mr={24} mb={24}>
+      <ToggleLink stretch>Active</ToggleLink>
+      <ToggleLink selected stretch>Inactive</ToggleLink>
+    </Toggle>
+    {DemoCodeBlock({code: 
 `<Toggle size="medium" stretch>
   <ToggleLink selected stretch>Active</ToggleLink>
   <ToggleLink stretch>Inactive</ToggleLink>
 </Toggle>`})}
 
     <demo.SmallLabel>Large Toggle with stretch prop</demo.SmallLabel>
-      <Toggle size="large" stretch mr={24} mb={24}>
-        <ToggleLink selected stretch >Active</ToggleLink>
-        <ToggleLink stretch >Inactive</ToggleLink>
-      </Toggle>
-      {DemoCodeBlock({code:
+    <Toggle size="large" stretch mr={24} mb={24}>
+      <ToggleLink selected stretch >Active</ToggleLink>
+      <ToggleLink stretch >Inactive</ToggleLink>
+    </Toggle>
+    {DemoCodeBlock({code:
 `<Toggle size="large" stretch>
   <ToggleLink selected stretch>Active</ToggleLink>
   <ToggleLink stretch>Inactive</ToggleLink>
