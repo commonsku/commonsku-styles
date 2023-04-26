a00dc734fd1de3ecc32ecbd47ad1a0aacbc11c2f
Haseeb Ahmad <ahmad.has03@gmail.com>
2023-04-26 10:40:35 -0400

    moved svg to react svg components

--- src/@commonsku/styles/StarRating.tsx
+++ src/@commonsku/styles/StarRating.tsx
@@ -1,22 +1,25 @@
-import styled from 'styled-components'
 import React from 'react'
-
-import star_dark from './star_dark.svg'
-import star_light from './star_light.svg'
+import styled from 'styled-components'
+import {
+  StarLightIcon,
+  StarDarkIcon
+} from './icons';
 
 import { SharedStyles, SharedStyleTypes } from './SharedStyles'
 
-const StyledStar = styled.img`
-  width: 13px;
-  height: 13px;
-  vertical-align: top;
-`
+const starStyles = {
+  width: 13,
+  height: 13,
+  verticalAlign: 'top',
+};
 const StyledRating = styled.div<SharedStyleTypes>`
   ${SharedStyles}
 `
 
 export const StarRating = (props: {rating:number} & SharedStyleTypes) => {
   return <StyledRating {...props}>
-    {[1,2,3,4,5].map((x,i) => i < props.rating ? <StyledStar key={`star${i}`} src={star_light}/> : <StyledStar key={`star${i}`} src={star_dark}/>)}
+    {[1,2,3,4,5].map((x,i) => i < props.rating ?
+      <StarLightIcon style={starStyles} /> :
+      <StarDarkIcon style={starStyles} />)}
   </StyledRating>
 }
