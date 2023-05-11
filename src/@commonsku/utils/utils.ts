9f74aba8548e7d1ba0dea1d313e0382362de51bc
Haseeb Ahmad <ahmad.has03@gmail.com>
2023-05-11 10:45:07 -0400

    moved some helper functions into utils from index.ts

---
+++ src/@commonsku/utils/utils.ts
@@ -0,0 +1,9 @@
+export const valIsValid = (val: any) => val !== null && val !== undefined && val !== ''; // not checking for 0
+
+export const aeval = (obj:{[key: string]: any}, key: string, def: any) => {
+    return obj && obj[key] ? obj[key] : def;
+}
+
+export function isClientSide() {
+  return Boolean(typeof window !== 'undefined' && window.document);
+}
