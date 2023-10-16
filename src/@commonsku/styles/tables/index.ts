791542abe6c988448073960c4f6206fd393094b5
Alexandra Bodak <abodak@abodak.local>
2023-10-16 15:05:07 -0400

    export SelectionTable everywhere

--- src/@commonsku/styles/tables/index.ts
+++ src/@commonsku/styles/tables/index.ts
@@ -3,3 +3,4 @@ export * from './SimpleWindowedTable';
 export { default as SimpleWindowedTable } from './SimpleWindowedTable';
 export { default as VirtualTableStyles } from './VirtualTableStyles';
 export { default as VirtualTable } from './VirtualTable';
+export { default as SelectionTable } from './SelectionTable';
