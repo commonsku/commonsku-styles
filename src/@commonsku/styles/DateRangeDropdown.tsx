7288cbc01044ab009501239d6840f4f98162b080
Alexandra Bodak <abodak@abodak.local>
2023-11-17 14:16:12 -0500

    pull DateRangeDropdown into a separate component, rewrite DateRange API

---
+++ src/@commonsku/styles/DateRangeDropdown.tsx
@@ -0,0 +1,121 @@
+import React, { CSSProperties, useEffect, useRef, useState } from 'react';
+import { Input, InputProps } from './Input';
+import { CalendarIcon } from './icons';
+import { format } from 'date-fns';
+import DateRangePicker, { DateRange, DateRangePickerProps } from './DateRangePicker';
+
+const dropdownStyles: CSSProperties = {
+    position: 'absolute',
+    marginTop: '1rem',
+    padding: '1rem',
+    background: '#fff',
+    border: '2px solid',
+    borderRadius: '5px',
+    borderColor: 'var(--color-primary1-60)',
+    zIndex: '1',
+};
+
+const formatDateRange = ({startDate, endDate}: DateRange, dateFormat: string) => 
+    (startDate == null || endDate == null)
+        ? ''
+        : `${format(startDate, dateFormat)} to ${format(endDate, dateFormat)}`;
+
+interface DateRangeInputProps extends Omit<InputProps, 'onChange'> {
+    isClearable: boolean
+    selected: DateRange
+    dateFormat: string
+    onInputSelect: () => void
+};
+
+export const DateRangeInput = ({
+    error,
+    noMargin,
+    onClick,
+    isClearable,
+    selected,
+    dateFormat,
+    onInputSelect,
+    ...props
+}: DateRangeInputProps) => {
+    const [value, setValue] = useState<string>();
+
+    useEffect(() => {
+        setValue(formatDateRange(selected, dateFormat));
+    }, [dateFormat, selected]);
+
+    return (
+        <div style={{ position: 'relative', display: 'inline-block' }}>
+            <Input
+                onFocus={onInputSelect}
+                value={value}
+                onClick={onClick}
+                noMargin={noMargin}
+                error={error}
+                style={{ marginRight: '2rem', display: 'inline-block', width: '250px' }}
+                type="text"
+                autoComplete="off"
+                {...props}
+            />
+            <span 
+                style={{
+                    fontStyle: 'normal',
+                    cursor: 'pointer',
+                    position: 'absolute',
+                    top: '8px',
+                    right: '36px',
+                }} 
+                onClick={onClick}
+            >
+                {!isClearable &&
+                    <CalendarIcon style={{ width: '1.9rem', verticalAlign: 'middle' }}/> 
+                }
+            </span>
+        </div>
+    );
+};
+
+export const DateRangeDropdown = (props: DateRangePickerProps) => {
+    const {
+        range,
+        error,
+        isClearable=false,
+        dateFormat='yyyy-MM-dd',
+        placeholder,
+        placeholderText,
+        style=dropdownStyles,
+    } = props;
+
+    const [open, setOpen] = useState(false);
+    const datepickerRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const handleExternalClick = (e: MouseEvent) => {
+            if (datepickerRef.current != null && !datepickerRef.current.contains(e.target as Node)) {
+                setOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleExternalClick);
+
+        return () => document.removeEventListener("mousedown", handleExternalClick);
+    }, []);
+
+    return (
+        <>
+            <DateRangeInput
+                noMargin
+                onInputSelect={() => setOpen(true)}
+                error={error}
+                isClearable={isClearable}
+                selected={range}
+                dateFormat={dateFormat}
+                placeholder={placeholder || placeholderText || `${dateFormat} to ${dateFormat}`}
+            />
+            {open && 
+                <DateRangePicker ref={datepickerRef} style={style} {...props} />
+            }
+        </>
+    );
+}
+
+export default DateRangePicker;
--- src/@commonsku/styles/DateRangePicker.tsx
+++ src/@commonsku/styles/DateRangePicker.tsx
@@ -1,22 +1,10 @@
-import React, { CSSProperties, SyntheticEvent, useCallback, useEffect, useRef, useState } from 'react';
+import React, { CSSProperties, SyntheticEvent, forwardRef, useCallback, useState } from 'react';
 import Datepicker, { DatepickerProps } from './Datepicker';
-import { Input, InputProps, LabeledRadioInButton } from './Input';
-import { CalendarIcon } from './icons';
-import { format } from 'date-fns';
+import { LabeledRadioInButton } from './Input';
+
 import { Text } from './Text';
 import { Col, Row } from './FlexboxGrid';
 import colors from './colors';
-
-const dropdownStyles: CSSProperties = {
-    position: 'absolute',
-    marginTop: '1rem',
-    padding: '1rem',
-    background: '#fff',
-    border: '2px solid',
-    borderRadius: '5px',
-    borderColor: 'var(--color-primary1-60)',
-    zIndex: '1',
-};
 
 const tabStyles: CSSProperties = {
     fontSize: '20px',
@@ -41,73 +29,15 @@ const presetListStyles: CSSProperties = {
     gap: '0.5rem 1rem',
 }
 
-export type DateRange = [Date | null, Date | null]
-
-const formatDateRange = ([start, end]: DateRange, dateFormat: string) => 
-    (start == null || end == null)
-        ? ''
-        : `${format(start, dateFormat)} to ${format(end, dateFormat)}`;
-
-interface DateRangeInputProps extends Omit<InputProps, 'onChange'> {
-    isClearable: boolean
-    selected: DateRange
-    dateFormat: string
-    onInputSelect: () => void
-};
-
-export const DateRangeInput = ({
-    error,
-    noMargin,
-    onClick,
-    isClearable,
-    selected,
-    dateFormat,
-    onInputSelect,
-    ...props
-}: DateRangeInputProps) => {
-    const [value, setValue] = useState<string>();
-
-    useEffect(() => {
-        setValue(formatDateRange(selected, dateFormat));
-    }, [dateFormat, selected]);
-
-    return (
-        <div style={{ position: 'relative', display: 'inline-block' }}>
-            <Input
-                onFocus={onInputSelect}
-                value={value}
-                onClick={onClick}
-                noMargin={noMargin}
-                error={error}
-                style={{ marginRight: '2rem', display: 'inline-block', width: '250px' }}
-                type="text"
-                autoComplete="off"
-                {...props}
-            />
-            <span 
-                style={{
-                    fontStyle: 'normal',
-                    cursor: 'pointer',
-                    position: 'absolute',
-                    top: '8px',
-                    right: '36px',
-                }} 
-                onClick={onClick}
-            >
-                {!isClearable &&
-                    <CalendarIcon
-                        style={{ width: '1.9rem', verticalAlign: 'middle' }}
-                        onClick={onInputSelect}
-                    /> 
-                }
-            </span>
-        </div>
-    );
-};
+export interface DateRange {
+    category: string
+    startDate?: Date | null
+    endDate?: Date | null
+}
 
 export interface DateRangePreset {
-    label: string,
-    onSelect: (preset: DateRangePreset) => void,
+    name: string
+    label: string
 }
 
 export type DateRangePickerProps = Omit<DatepickerProps, 'value' | 'onChange' | 'dateFormat'> & { 
@@ -120,55 +50,43 @@ export type DateRangePickerProps = Omit<DatepickerProps, 'value' | 'onChange' | 
     presets?: DateRangePreset[]
 }
 
-export const DateRangePicker = ({
-    range,
-    onChange,
-    presets,
-    error,
-    customInput,
-    locale='en',
-    todayButton='Today',
-    placeholder,
-    placeholderText,
-    dateFormat='yyyy-MM-dd',
-    isClearable=false,
-    peekNextMonth=true,
-    showMonthDropdown=true,
-    showYearDropdown=true,
-    showPopperArrow=false,
-    dropdownMode="select",
-    nextMonthButtonLabel="",
-    nextYearButtonLabel="",
-    previousMonthButtonLabel="",
-    previousYearButtonLabel="",
-    popperClassName,
-    wrapperClassName,
-    ...props
-}: DateRangePickerProps) => {
-    const [startDate, endDate] = range;
-    const [open, setOpen] = useState(false);
+export const DateRangePicker = forwardRef<HTMLDivElement, DateRangePickerProps>((
+    {
+        range,
+        onChange,
+        presets,
+        error,
+        customInput,
+        locale='en',
+        todayButton='Today',
+        placeholder,
+        placeholderText,
+        dateFormat='yyyy-MM-dd',
+        isClearable=false,
+        peekNextMonth=true,
+        showMonthDropdown=true,
+        showYearDropdown=true,
+        showPopperArrow=false,
+        dropdownMode="select",
+        nextMonthButtonLabel="",
+        nextYearButtonLabel="",
+        previousMonthButtonLabel="",
+        previousYearButtonLabel="",
+        popperClassName,
+        wrapperClassName,
+        style,
+        ...props
+    },
+    ref
+) => {
+    const { startDate, endDate } = range;
     const [activeTab, setActiveTab] = useState<'custom' | 'preset'>('custom');
     const [selectedPreset, setSelectedPreset] = useState<DateRangePreset>();
-    const dropdownRef = useRef<HTMLDivElement | null>(null);
 
     const hasPresets = presets != null && presets.length > 0;
 
-    useEffect(() => {
-        const handleExternalClick = (e: MouseEvent) => {
-            if (dropdownRef.current != null && !dropdownRef.current.contains(e.target as Node)) {
-                setOpen(false);
-            }
-        }
-
-        document.addEventListener("mousedown", handleExternalClick);
-
-        return () => document.removeEventListener("mousedown", handleExternalClick);
-    }, []);
-
     const handleChange = useCallback(
-        (selected: 'start' | 'end', range: DateRange, event?: SyntheticEvent<any>) => {
-            let [newStart, newEnd] = range;
-
+        (selected: 'start' | 'end', newStart?: Date | null, newEnd?: Date | null, event?: SyntheticEvent<any>) => {
             if (newStart != null && newEnd != null && newStart > newEnd) {
                 if (selected === 'start') {
                     newEnd = newStart;
@@ -178,7 +96,13 @@ export const DateRangePicker = ({
             }
 
             if (onChange != null) {
-                onChange([newStart, newEnd], event);
+                const newDateRange: DateRange = {
+                    category: "custom",
+                    startDate: newStart,
+                    endDate: newEnd,
+                };
+
+                onChange(newDateRange, event);
             }
         },
         [onChange]
@@ -186,11 +110,13 @@ export const DateRangePicker = ({
 
     const handleSelectPreset = useCallback((preset: DateRangePreset) => {
         setSelectedPreset(preset);
-        preset.onSelect(preset);
-    }, [setSelectedPreset]);
+        if (onChange != null) {
+            onChange({ category: preset.name });
+        }
+    }, [onChange, setSelectedPreset]);
 
     const renderCustomTab = () => (
-        <Row style={{ gap: '2rem' }}>
+        <Row style={{ flexFlow: 'row', gap: '2rem' }}>
             <Col>
                 <div style={{ marginBottom: '0.25rem' }}>
                     From
@@ -202,7 +128,7 @@ export const DateRangePicker = ({
                         selected={startDate}
                         dateFormat={dateFormat}
                         isClearable={isClearable}
-                        onChange={(newStart, event) => handleChange('start', [newStart, endDate], event)}
+                        onChange={(newStart, event) => handleChange('start', newStart, endDate, event)}
                     />
                     <Datepicker
                         inline
@@ -220,7 +146,7 @@ export const DateRangePicker = ({
                         dropdownMode={dropdownMode}
                         peekNextMonth={peekNextMonth}
                         calendarClassName={`commonsku-styles-datepicker ${popperClassName || ''}`}
-                        onChange={(newStart, event) => handleChange('start', [newStart, endDate], event)}
+                        onChange={(newStart, event) => handleChange('start', newStart, endDate, event)}
                         {...props}
                     />
                 </div>
@@ -236,7 +162,7 @@ export const DateRangePicker = ({
                         selected={endDate}
                         dateFormat={dateFormat}
                         isClearable={isClearable}
-                        onChange={(newEnd, event) => handleChange('end', [startDate, newEnd], event)}
+                        onChange={(newEnd, event) => handleChange('end', startDate, newEnd, event)}
                     />
                     <Datepicker
                         inline
@@ -254,7 +180,7 @@ export const DateRangePicker = ({
                         dropdownMode={dropdownMode}
                         peekNextMonth={peekNextMonth}
                         calendarClassName={`commonsku-styles-datepicker ${popperClassName || ''}`}
-                        onChange={(newEnd, event) => handleChange('end', [startDate, newEnd], event)}
+                        onChange={(newEnd, event) => handleChange('end', startDate, newEnd, event)}
                         {...props}
                     />
                 </div>
@@ -277,39 +203,26 @@ export const DateRangePicker = ({
     );
 
     return (
-        <>
-            <DateRangeInput
-                noMargin
-                onInputSelect={() => setOpen(true)}
-                error={error}
-                isClearable={isClearable}
-                selected={range}
-                dateFormat={dateFormat}
-                placeholder={placeholder || placeholderText || `${dateFormat} to ${dateFormat}`}
-            />
-            {open &&
-                <div ref={dropdownRef} style={dropdownStyles}>
-                    {hasPresets &&
-                        <Row style={{ gap: '2rem', marginBottom: '2rem' }}>
-                            <Text
-                                style={activeTab === 'custom' ? activeTabStyles : tabStyles}
-                                onClick={() => setActiveTab('custom')}
-                            >
-                                Custom
-                            </Text>
-                            <Text
-                                style={activeTab === 'preset' ? activeTabStyles : tabStyles}
-                                onClick={() => setActiveTab('preset')}
-                            >
-                                Preset
-                            </Text>
-                        </Row>
-                    }
-                    {activeTab === 'custom' ? renderCustomTab() : renderPresetTab()}
-                </div>     
+        <div ref={ref} style={style}>
+            {hasPresets &&
+                <Row style={{ gap: '2rem', marginBottom: '2rem' }}>
+                    <Text
+                        style={activeTab === 'custom' ? activeTabStyles : tabStyles}
+                        onClick={() => setActiveTab('custom')}
+                    >
+                        Custom
+                    </Text>
+                    <Text
+                        style={activeTab === 'preset' ? activeTabStyles : tabStyles}
+                        onClick={() => setActiveTab('preset')}
+                    >
+                        Preset
+                    </Text>
+                </Row>
             }
-        </>
+            {activeTab === 'custom' ? renderCustomTab() : renderPresetTab()}
+        </div>     
     );
-}
+});
 
 export default DateRangePicker;
