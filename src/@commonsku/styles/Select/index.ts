6e0c769b894ea5df3c287ed2c1b2e56e5552552c
Haseeb Ahmad <ahmad.has03@gmail.com>
2024-01-05 11:35:49 -0500

    updated select exports

--- src/@commonsku/styles/Select/index.ts
+++ src/@commonsku/styles/Select/index.ts
@@ -1,13 +1,4 @@
-import { components as baseComponents } from 'react-select';
-
 export { LabeledSelect, SKUSelect as Select } from './Select';
 export { LabeledAsyncSelect, SKUAsyncSelect as AsyncSelect } from './AsyncSelect';
 export { LabeledCreatableSelect, SKUCreatableSelect as CreatableSelect } from './CreatableSelect';
-export { createFilter, mergeStyles } from 'react-select';
-
-export const components = {
-  ...baseComponents,
-  MenuListHeader: null,
-  MenuListFooter: null,
-};
-
+export { createFilter, mergeStyles, components } from 'react-select';
