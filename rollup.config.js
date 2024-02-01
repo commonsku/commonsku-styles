1b621ea2b43cf54dfce3be6adf481ff7a6693f94
Haseeb Ahmad <ahmad.has03@gmail.com>
2024-02-01 11:42:19 -0500

    code splitting with rollup - split each folder with index.ts in styles/ dir to it's own package - existing code should move components to their own directories like Input as many components in a file - new components should be created in their own directories - add eslint for fix lint errors in rollup js files - update and optimize rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -15,6 +15,9 @@ import svgr from '@svgr/rollup';
 import typescript from 'rollup-plugin-typescript2';
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import { getFiles } from './scripts/buildUtils.js';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -23,63 +26,130 @@ const dateFnsDirs = fs
   .readdirSync(path.join(".", "node_modules", "date-fns"))
   .map((d) => `date-fns/${d}`);
 
+const plugins = [
+  peerDepsExternal({ includeDependencies: true }),
+  progress(),
+  image(),
+  url({
+    include: [
+      '**/*.eot', '**/*.otf', '**/*.ttf', '**/*.woff', '**/*.woff2', 
+      '**/*.png', '**/*.jpg', '**/*.gif'
+    ],
+  }),
+  svgr(),
+  commonjs({
+    include: ['node_modules/**'],
+    exclude: ['node_modules/process-es6/**'],
+  }),
+  postcss({
+    modules: true
+  }),
+  resolve(),
+  babel({
+    exclude: 'node_modules/**',
+    babelHelpers: 'runtime'
+  }),
+  filesize(),
+  typescript({
+    tsconfig: 'tsconfig.lib.json',
+    useTsconfigDeclarationDir: true,
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+    preventAssignment: true,
+    __IS_DEV__: process.env.NODE_ENV === 'development',
+  }),
+  terser(),
+];
+
+const subfolderPlugins = (folderName) => [
+  ...plugins,
+  generatePackageJson({
+    baseContents: {
+      name: `${pkg.name}/${folderName.toLowerCase()}`,
+      private: true,
+      main: "./index.cjs",
+      module: "./index.mjs",
+      types: "./index.d.ts",
+      peerDependencies: pkg.peerDependencies,
+      version: pkg.version,
+    },
+    outputFolder: `dist/${folderName}/`
+  }),
+];
+
+const folderBuilds = fs.readdirSync('./src/@commonsku/styles').map((folder) => {
+  if (!fs.existsSync(`src/@commonsku/styles/${folder}/index.ts`)) {
+    return null;
+  }
+
+  return {
+    input: `src/@commonsku/styles/${folder}/index.ts`,
+    output: [{
+      file: `dist/${folder}/index.mjs`,
+      format: 'es',
+      exports: 'named',
+    }, {
+      file: `dist/${folder}/index.cjs`,
+      format: 'cjs',
+      exports: 'named',
+    }],
+    plugins: subfolderPlugins(folder),
+    external: Object.keys(pkg.dependencies)
+      .concat(Object.keys(pkg.peerDependencies))
+      .concat(dateFnsDirs)
+      .concat([/node_modules/]),
+  };
+}).filter(v => v);
+
+const folderTypes = fs.readdirSync('./src/@commonsku/styles').map((folder) => {
+  if (!fs.existsSync(`src/@commonsku/styles/${folder}/index.ts`)) {
+    return null;
+  }
+
+  return {
+    input: `dist/styles/${folder}/index.d.ts`,
+    output: [{ file: `dist/${folder}/index.d.ts`, format: "es" }],
+    plugins: [dts()],
+  };
+}).filter(v => v);
+
 const config = [
+  {
+    input: getFiles('src/@commonsku/utils', ['js', 'ts', 'tsx']),
+    output: {
+      dir: 'dist/utils/',
+      format: 'es',
+    },
+    plugins: plugins,
+    external: [/node_modules/],
+  },
   {
     input: 'src/@commonsku/styles/index.ts',
     output: [{
       file: pkg.main,
       format: 'cjs',
-      // exports: 'named',
-      sourcemap: true,
+      exports: 'named',
       interop: 'compat',
     }, {
       file: pkg.module,
       format: 'es',
-      // exports: 'named',
-      sourcemap: true,
+      exports: 'named',
       interop: 'compat',
     }],
-    plugins: [
-      peerDepsExternal({ includeDependencies: true }),
-      progress(),
-      image(),
-      url({
-        include: [
-          '**/*.eot', '**/*.otf', '**/*.ttf', '**/*.woff', '**/*.woff2', 
-          '**/*.png', '**/*.jpg', '**/*.gif'
-        ],
-      }),
-      svgr(),
-      commonjs({
-        include: ['node_modules/**'],
-        exclude: ['node_modules/process-es6/**'],
-      }),
-      postcss({
-        modules: true
-      }),
-      resolve(),
-      babel({
-        exclude: 'node_modules/**',
-        babelHelpers: 'runtime'
-      }),
-      filesize(),
-      typescript({
-        tsconfig: 'tsconfig.lib.json',
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        preventAssignment: true,
-      }),
-    ],
+    plugins,
     external: Object.keys(pkg.dependencies)
       .concat(Object.keys(pkg.peerDependencies))
-      .concat(dateFnsDirs),
+      .concat(dateFnsDirs)
+      .concat([/node_modules/]),
   },
+  ...folderBuilds,
   {
     input: "dist/styles/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
   },
+  ...folderTypes,
 ];
 
 export default config;
