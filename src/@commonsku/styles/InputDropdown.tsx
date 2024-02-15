b22a5e0252b6b4335598e8484a77130e04d030c0
Haseeb Ahmad <ahmad.has03@gmail.com>
2024-02-15 12:06:42 -0500

    update styles with keyboard navigation

--- src/@commonsku/styles/InputDropdown.tsx
+++ src/@commonsku/styles/InputDropdown.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState, } from 'react';
-import styled from 'styled-components';
+import styled, { CSSProperties } from 'styled-components';
 import Csku from './Csku';
 import DebouncedInput, { DebouncedInputProps } from './DebouncedInput';
 
@@ -38,7 +38,7 @@ const OptionsList = styled.div`
 &&& {
   position: absolute;
   z-index: 1;
-  margin-top: 70px;
+  margin-top: 40px;
   width: 100%;
   background-color: white;
   display: flex;
@@ -71,6 +71,7 @@ const DropdownOption = ({
 type TBaseInputDropdownOption = {
   label: string;
   value: string;
+  style?: CSSProperties,
 };
 type TInputDropdownOption<T extends TBaseInputDropdownOption = TBaseInputDropdownOption> = T;
 export type InputDropdownProps<
@@ -81,6 +82,9 @@ export type InputDropdownProps<
   onSelectOption?: (v: TInputDropdownOption<T>) => void;
   extraOptions?: React.ReactNode;
   isOpen?: boolean;
+  wrapperStyle?: CSSProperties;
+  searchWrapperStyle?: CSSProperties;
+  optionsListStyle?: CSSProperties;
 };
 const ForwardedInputDropdown = <
   L extends boolean = false,
@@ -95,11 +99,15 @@ const ForwardedInputDropdown = <
     onChange,
     isOpen,
     value: initialValue = '',
+    wrapperStyle = {},
+    searchWrapperStyle = {},
+    optionsListStyle = {},
     ...rest
   }: Omit<InputDropdownProps<L, T>, 'ref'>,
   ref?: React.ForwardedRef<HTMLInputElement>
 ) => {
   const rootRef = useRef<HTMLDivElement>(null);
+  const [keyOptionIdx, setKeyOptionIdx] = useState(-1);
   const [showDropdown, setShowDropdown] = useState(isOpen || false);
   const [value, setValue] = useState(initialValue);
 
@@ -134,9 +142,32 @@ const ForwardedInputDropdown = <
     }
   }, [options]);
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
+    if (options.length === 0) {
+      return;
+    }
+
+    const key = e.key;
+    if (key === 'ArrowDown') {
+      setKeyOptionIdx(
+        s => s >= options.length-1 ? 0 : s+1
+      );
+    } else if (key === 'ArrowUp') {
+      setKeyOptionIdx(
+        s => s <= 0 ? options.length-1 : s-1
+      );
+    } else if (key === 'Enter'
+      && keyOptionIdx >= 0
+      && keyOptionIdx <= options.length
+    ) {
+      onSelectOption?.(options[keyOptionIdx]);
+      setKeyOptionIdx(-1);
+    }
+  }
+
   return (
-    <Wrapper ref={rootRef}>
-      <SearchWrapper>
+    <Wrapper ref={rootRef} style={wrapperStyle}>
+      <SearchWrapper style={searchWrapperStyle}>
         <DebouncedInput
           {...rest}
           ref={ref}
@@ -145,22 +176,33 @@ const ForwardedInputDropdown = <
           value={value}
           wrapperProps={{ style: { width: '100%', } }}
           onChange={v => {
+            setKeyOptionIdx(-1);
             setValue(v);
             onChange?.(v);
           }}
+          onClick={(e) => { e.currentTarget.readOnly = false; }}
+          onFocus={(e) => { e.currentTarget.readOnly = false; }}
+          onKeyDown={onKeyDown}
         />
       </SearchWrapper>
-      {!showDropdown ? null : <OptionsList>
-        {options.map(op => (
+      {showDropdown ? <OptionsList style={optionsListStyle}>
+        {options.map((op, i) => (
           <DropdownOption
             label={op.label}
             value={op.value}
+            style={{
+              ...(keyOptionIdx === i ? {
+                backgroundColor: '#efefef',
+              } : {}),
+              ...op.style
+            }}
             key={`option-${op.value}`}
             onClick={() => onSelectOption?.(op)}
+            onMouseOver={() => setKeyOptionIdx(i)}
           />
         ))}
         {extraOptions}
-      </OptionsList>}
+      </OptionsList> : null}
       {children}
     </Wrapper>
   );
