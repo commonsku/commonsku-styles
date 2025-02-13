bfcba6006070179f57f3334cd3a8e7fd1b93310c
Haseeb Ahmad <ahmad.has03@gmail.com>
2025-02-13 10:28:39 -0500

    replaced clip-path to clipPath

--- src/@commonsku/styles/icons/UnsplashIcon.tsx
+++ src/@commonsku/styles/icons/UnsplashIcon.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { getSvgSizeStyles, SVGIconProps } from './SvgIcon';
 import { teal } from '../colors';
+import { uniqueId } from 'lodash';
 
 type UnsplashIconProps = SVGIconProps & { color?: string };
 export default function UnsplashIcon({
@@ -20,13 +21,14 @@ export default function UnsplashIcon({
         }),
         [size, props.width, props.height, props.iconSizes, props.pointer, props.viewBox]
     );
+    const clipId = uniqueId('clip0_UnsplashIcon');
     return <svg {...props} aria-labelledby="UnsplashIcon" viewBox={viewBox} style={{...style, ...props.style}}>
         <title id="UnsplashIcon">{altText}</title>
-        <g clip-path="url(#clip0_1807_14536)">
+        <g clipPath={`url(#${clipId})`}>
             <path d="M6.25 5.625V0H13.75V5.625H6.25ZM13.75 8.75H20V20H0V8.75H6.25V14.375H13.75V8.75Z" fill={color} />
         </g>
         <defs>
-            <clipPath id="clip0_1807_14536">
+            <clipPath id={clipId}>
                 <rect width={style.width || 20} height={style.height || 20} fill="white" />
             </clipPath>
         </defs>
